a9e39f21ad324c9adbdb0a4a0b785bce26111ba8
ChandraShekharSaini <chandrashekharsaini322@gmail.com>
2025-06-26 22:03:36 +0530

    Added Changes

--- server/index.js
+++ server/index.js
@@ -1,59 +1,68 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/listing.route.js'
-import userRouter from './routes/user.route.js'
-import cookieParser from 'cookie-parser'
-import path from 'path'
-import dotenv from 'dotenv'
-import jwt from "jsonwebtoken"
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+import userRouter from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 
 const app = express();
-const PORT = process.env.PORT || 4444
-dotenv.config()
+const PORT = process.env.PORT || 4444;
+dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
+import googlePassport from "./authentication/googleAuth.js";
+app.use(googlePassport.initialize());
+app.get(
+  "/auth/google",
+  googlePassport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
 
-
-import googlePassport from "./authentication/googleAuth.js"
-app.use(googlePassport.initialize())
-app.get('/auth/google',
-  googlePassport.authenticate('google', {
-    scope:
-      ['email', 'profile']
-  }
-  ));
-
-app.get('/auth/google/callback',
-  googlePassport.authenticate('google', { failureRedirect: 'https://mern-estate-project-tles.onrender.com/sign-in', session: false }),
+app.get(
+  "/auth/google/callback",
+  googlePassport.authenticate("google", {
+    failureRedirect: "https://mern-estate-project-tles.onrender.com/sign-in",
+    session: false,
+  }),
   function (req, res) {
+    const token = jwt.sign({ user: req.user }, "jhggyytftyfmm", {
+      expiresIn: "1h",
+    });
+    res
+      .status(200)
+      .redirect(
+        `https://mern-estate-project-tles.onrender.com/redirect-to-home?user=${encodeURIComponent(
+          JSON.stringify(token)
+        )}`
+      );
+  }
+);
 
-    const token = jwt.sign({ user: req.user }, "jhggyytftyfmm", { expiresIn: '1h' });
-    res.status(200).redirect(`https://mern-estate-project-tles.onrender.com/redirect-to-home?user=${encodeURIComponent(JSON.stringify(token))}`);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
-  });
-
-
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter)
-app.use('/api/listing', listingRouter);
-
-app.get("/",(req,res,next)=>{
-  res.send("My Mern  Estate Project")
-})
+app.get("/", (req, res, next) => {
+  res.send("My Mern  Estate Project");
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
@@ -61,29 +70,20 @@ app.use((err, req, res, next) => {
   });
 });
 
-
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-})
-
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// });
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log('Connected to DB');
+    console.log("Connected to DB");
 
+    app.listen(PORT, () => {
+      console.log("http://localhost:" + PORT);
+    });
   })
   .catch((error) => {
     console.log(error);
   });
-
-
-
-app.listen(PORT, () => {
-  console.log('http://localhost:' + PORT);
-});
-
-
-
-
